5c72cc6c50827ac6067604dd060f6a5092480c81
wreale <38162186+wreale@users.noreply.github.com>
2023-04-27 18:22:01 -0400

    added trim picker functionality

--- components/TrimPicker.js
+++ components/TrimPicker.js
@@ -1,22 +1,47 @@
+import { motion, AnimatePresence } from "framer-motion"
+import { useState } from 'react';
+
 import Heading from './Heading';
 import Image from 'next/image';
+
 const TrimPicker = ({ trimLevels }) => {
+    // [stateVariable, setterFunctionVariable]
+    const [activeIndex, setActiveIndex] = useState(0);
     return <div>
+       
+        <AnimatePresence>
+            <motion.div 
+            key={trimLevels[activeIndex].images.large.sourceUrl}
+            initial={{ x: 50, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            exit={{ x: -50, opacity: 0 }}
+            >
+            </motion.div>
+        </AnimatePresence>
+
+        <Image
+            src = {trimLevels[activeIndex].images.large.sourceUrl}
+            alt = {trimLevels[activeIndex].images.large.altText}
+            width = {trimLevels[activeIndex].images.large.mediaDetails.width}
+            height = {trimLevels[activeIndex].images.large.mediaDetails.height}
+        />
+        <Heading level={2}>{trimLevels[activeIndex].name}</Heading>
         <ul>
         {trimLevels.map((trimLevel, index) => {
             const {name, images} = trimLevel;
             const {thumbnail} = images;
-            return <li>
+            return <li onClick={ () => {
+                setActiveIndex(index)
+            }}>
                 <Heading level={3}>{name}</Heading>
                 {thumbnail &&
                     <Image 
-                    src={thumbnail.sourceUrl}
-                    alt={thumbnail.altText}
-                    width={thumbnail.mediaDetails.width}
-                    height={thumbnail.mediaDetails.height}
-                />
+                        src={thumbnail.sourceUrl}
+                        alt={thumbnail.altText}
+                        width={thumbnail.mediaDetails.width}
+                        height={thumbnail.mediaDetails.height}
+                    />
                 }
-                
             </li>
         })}
         </ul>
